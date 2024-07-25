264f4c062920d2fa3a9a511030ec3f3c756bd9a8
Chang Oh <72629686+chang4oh@users.noreply.github.com>
2024-07-25 10:02:38 -0500

    modfiy node

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
-/*App.js*/
-
-import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+// App.js
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 function App() {
-    const responseMessage = (response) => {
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log(error);
-    };
-    return (
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json',
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
         <div>
-            <h2>React Google Login</h2>
-            <br />
-            <br />
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
         </div>
-    )
+      ) : (
+        <button onClick={login}>Sign in with Google 🚀 </button>
+      )}
+    </div>
+  );
 }
+
 export default App;
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-/*index.js*/
-
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App';
+
+const clientId = "511076543925-hqst3kpifqmd518i3ff0j31ilokcf1tk.apps.googleusercontent.com";
 
 ReactDOM.render(
-    <GoogleOAuthProvider clientId="511076543925-hqst3kpifqmd518i3ff0j31ilokcf1tk.apps.googleusercontent.com">
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </GoogleOAuthProvider>,
-    document.getElementById('root')
+  <GoogleOAuthProvider clientId={clientId}>
+    <App />
+  </GoogleOAuthProvider>,
+  document.getElementById('root')
 );
